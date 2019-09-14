44d0544844695cce00005da8a99cbfe30dbec423
rene <rene.mroger@gmail.com>
2019-09-14 12:15:31 -0700

    componenets boilerPlate

---
+++ react-front-end/src/componenets/LoadingPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default LoadingPage(pros => {
+  return <></>;
+});
---
+++ react-front-end/src/componenets/SelectPickup.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default SelectPickup(pros => {
+  return <></>;
+});
