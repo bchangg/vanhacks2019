7351fb47c73967b774bf3b15284444f8236a85af
Marcelo Gaia <marcelozakk07@gmail.com>
2019-09-14 18:14:00 -0700

    Created a simple users api call, to start sending dummy calls.

--- express-back-end/routes/index.js
+++ express-back-end/routes/index.js
@@ -2,4 +2,6 @@ module.exports = function( App ) {
 	App.use( '/api/v1', require( './posts' ) );
 	App.use( '/api/v1', require( './org' ) );
 	App.use( '/api/v1', require( './pickups' ) );
+
+	App.use( '/', require( './users' ) );
 };
