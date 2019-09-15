cd5c245d793fb9c8e1e6faac2691ab8de64fa2ee
changbhc@gmail.com <changbhc@gmail.com>
2019-09-14 18:16:59 -0700

    login validation and functionality works, pushing before pulling from master for new routes

--- react-front-end/src/components/NgoSignIn.js
+++ react-front-end/src/components/NgoSignIn.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { navigate } from 'hookrouter'
+
+const useStyles = makeStyles(theme => ({
+  error: {
+    color: "red",
+    fontSize: "0.8rem",
+  }
+}));
 
 export default function NgoSignIn(props) {
+  const classes = useStyles();
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [error, setError] = useState({
+    email: '',
+    password: ''
+  });
+
+  const validate = function() {
+    if (!email) {
+      setError(current => ({
+        ...current,
+        email: 'Email cannot be empty!'
+      }))
+    }
+    if (!password) {
+      setError(current => ({
+        ...current,
+        password: 'Please enter your password!'
+      }))
+    }
+  }
+
+  const login = function(event) {
+    event.preventDefault();
+    validate();
+    console.log(email, password);
+  };
+
   return (
-    <div>
-      <p>NGO SIGN IN</p>
+    <div className="d-flex flex-column align-items-center">
+      <p>NGO Sign-In</p>
+      <form onSubmit={event => login(event)} className="d-flex flex-column align-items-center">
+        <TextField
+          error={email === ''}
+          type="email"
+          label="Email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+        <span className={classes.error}>{error.email}</span>
+        <TextField
+          error={password === ''}
+          type="password"
+          label="Password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+        <span className={classes.error}>{error.password}</span>
+        <Button type="submit">Log In</Button>
+      </form>
     </div>
   )
 }
