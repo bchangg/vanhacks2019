ebcd21feba86acbe2fb633a02d226072d452d8c7
Marcelo Gaia <marcelozakk07@gmail.com>
2019-09-14 20:57:12 -0700

    Implemented the 'create item/post' api call.

--- express-back-end/routes/posts.js
+++ express-back-end/routes/posts.js
@@ -1,9 +1,9 @@
-const express = require( 'express' );
+const express = require('express');
 const router = express.Router();
 const knexfile = require('../knexfile');
-const knex = require('knex')( knexfile.test );
+const knex = require('knex')(knexfile.test);
 
-router.get( '/posts', ( req, res ) => {
+router.get('/posts', (req, res) => {
     knex
         .select([
             'posts.id',
@@ -17,20 +17,42 @@ router.get( '/posts', ( req, res ) => {
         ])
         .from('posts')
         .leftJoin('post_images', 'posts.id', 'post_images.post_id')
-        .then( result => {
-            res.json( result );
+        .then(result => {
+            res.json(result);
         });
 });
 
-router.get( '/posts/:postId', ( req, res ) => {
+router.post('/posts', (req, res) => {
+    let postObj = {
+        title: req.body.title,
+        quantity: req.body.quantity,
+        notes: req.body.notes,
+        pickup_deadline: req.body.pickup_deadline,
+        pickup_location: req.body.pickup_location,
+        phone: req.body.phone,
+        item_type: req.body.item_type
+    };
+
+    knex('posts')
+        .insert(postObj)
+        .then(results => {
+            if (results.rowCount && results.rowCount > 0) {
+                res.json( {
+                    message: "Post created with success!"
+                })
+            }
+        });
+});
+
+router.get('/posts/:postId', (req, res) => {
     let postId = req.params.postId;
 
     knex
         .select('*')
         .from('posts')
         .where({ 'id': postId })
-        .then( result => {
-            res.json( result );
+        .then(result => {
+            res.json(result);
         });
 });
 
